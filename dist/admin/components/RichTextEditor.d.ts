33dcb11cfbe89f58da41994945f4a39a82747a28
junior-boop <juniorseppo3@gmail.com>
2026-05-01 14:31:59 +0100

    fix: richtextEditor module

---
+++ dist/admin/components/RichTextEditor.d.ts
@@ -0,0 +1,8 @@
+interface Props {
+    name: string;
+    initialValue?: string;
+    placeholder?: string;
+}
+export default function RichTextEditor({ name, initialValue, placeholder }: Props): import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=RichTextEditor.d.ts.map
